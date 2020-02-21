25406d12d00a4b07ac14dee8b32a294bee0adefc
Riccardo Ciovati <riccardo.ciovati@soundcloud.com>
2020-02-21 21:20:29 +0100

    Export JAVA_HOME and ANDROID_SDK_ROOT

--- path.zsh
+++ path.zsh
@@ -5,3 +5,5 @@
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
 #export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+export JAVA_HOME=$(/usr/libexec/java_home)
+export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
